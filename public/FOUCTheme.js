852d89f4de6009e26d286d55f028421999bd89ed
shringo <143767857+shringo@users.noreply.github.com>
2025-08-09 00:16:44 -1000

    Fix FOUC again
    by setting dangerouslySetInnerHTML

--- public/FOUCTheme.js
+++
@@ -1,3 +0,0 @@
-const theme = localStorage.getItem("theme") ?? 'A';
-if(theme === 'D' || (theme === 'A' && window.matchMedia("(prefers-color-scheme: dark)").matches))
-  document.documentElement.classList.add("dark");
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,17 @@ export default function Document() {
   return <Html>
     <Head />
     <body>
-      <Script src="/FOUCTheme.js" strategy="beforeInteractive"/>
       <Main/>
       <NextScript/>
+      <Script
+        id="fouc-theme"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `const theme = localStorage.getItem("theme") ?? 'A';
+                  if(theme === 'D' || (theme === 'A' && window.matchMedia("(prefers-color-scheme: dark)").matches))
+                    document.documentElement.classList.add("dark");`
+        }}
+      />
     </body>
   </Html>;
 };
