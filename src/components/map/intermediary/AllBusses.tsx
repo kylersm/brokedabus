118b23cce4cf775263cedbf1233c666209b95f24
kyler <143767857+kylersm@users.noreply.github.com>
2026-01-03 12:29:45 -1000

    examine nextStop by only checking for some trips

--- src/components/map/intermediary/AllBusses.tsx
+++ src/components/map/intermediary/AllBusses.tsx
@@ -4,7 +4,7 @@ import { useMap, StopArrival } from "../mapIntermediate";
 import StopPopup from "../popups/StopPopup";
 import { ActiveType, type VehicleFiltering } from "../../Vehicles";
 import { filterVehicles } from "~/lib/BusTypes";
-import { areArraysSimilar, getHSTTime } from "~/lib/util";
+import { getHSTTime } from "~/lib/util";
 import VehicleFilterOptions from "../../VehicleFilterOptions";
 import { createPostRqVehicles, getExpectedStop, getVehicleNow } from "~/lib/GTFSBinds";
 
@@ -83,7 +83,7 @@ export function AllBusses() {
       vehicles={filteredVehicles?.map(v => {
         const info = v.tripInfo?.trips;
         return { ...v, nextStop: getExpectedStop(
-          allStops?.filter(s => areArraysSimilar(s.trips.map(t => t._id), info)).map(s => ({ stop: s.stop, trip: s.trips.find(t => info?.includes(t._id))! })), v.tripInfo, getVehicleNow(v, now)) 
+          allStops?.filter(s => s.trips.map(t => t._id).some(t => info?.includes(t))).map(s => ({ stop: s.stop, trip: s.trips.find(t => info?.includes(t._id))! })), v.tripInfo, getVehicleNow(v, now)) 
         };
       })}
       vehicleHook={setOpenVehicles}
